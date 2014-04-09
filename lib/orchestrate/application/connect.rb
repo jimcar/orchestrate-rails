6339dfb7f07f0d10fc010446832c9fcb48d01453
Justin Mecham <justin@mecham.me>
2014-04-09 17:47:11 -0400

    Changes to support orchestrate-api config changes.

--- lib/orchestrate/application/connect.rb
+++ lib/orchestrate/application/connect.rb
@@ -6,11 +6,11 @@ module Orchestrate::Application
   class Connect
 
     # default config file: "<app-root-dir>/orch_config.json"
-    @@config_file = "orch_config.json"
+    # @@config_file = "orch_config.json"
 
-    def self.config(config_file)
-      @@config_file = config_file
-    end
+    # def self.config(config_file)
+    #   @@config_file = config_file
+    # end
 
     def self.client
       @@client ||= connect
@@ -18,7 +18,8 @@ module Orchestrate::Application
 
     private
       def self.connect
-        Orchestrate::API::Wrapper.new @@config_file
+        Orchestrate::API::Wrapper.new
+        # Orchestrate::API::Wrapper.new @@config_file
       end
   end
 
